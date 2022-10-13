bb140733bd6420963e3d30183bb8b2222e00f818
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-13 08:32:58 -0300

    config ready

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import CoinStack from 'src/coin-stack'
+import {NavigationContainer} from '@react-navigation/native';
+import CoinStack from 'src/coin-stack';
 const App = () => {
-  
   return (
     <NavigationContainer>
-      <CoinStack/>
+      <CoinStack />
     </NavigationContainer>
   );
 };
-
 
 export default App;
