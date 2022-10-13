9d0e071245b01e1c9340d88e4eb61ff7bddcb6ba
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-12 23:54:06 -0300

    init project

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import CoinStack from 'src/coin-stack'
 const App = () => {
   
   return (
     <NavigationContainer>
-
+      <CoinStack/>
     </NavigationContainer>
   );
 };
--- src/components/coins/CoinScreen.js
+++ src/components/coins/CoinScreen.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {View, Text} from 'react-native'
 function CoinScreen() {
   return (
-    <div>CoinScreen</div>
+    <View>
+        <Text>
+            Coin Screen
+        <Text/>
+    </View>
   )
 }
 
