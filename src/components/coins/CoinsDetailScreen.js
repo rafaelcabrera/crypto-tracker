c037fca2094e810a3626f4664a2ca3d0ce8711d0
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-14 23:57:05 -0300

    btn added Coins Detail

---
+++ src/components/coins/CoinsDetailScreen.js
@@ -0,0 +1,38 @@
+import { View, Text , Pressable, StyleSheet} from "react-native";
+
+import React from "react";
+
+export const CoinsScreen = ({ navigation }) => {
+  const handlePess = () => {
+    navigation.navigate("CoinDetailScreen");
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>CoinsScreen </Text>
+      <Pressable style={styles.btn} onPress={handlePess}>
+        <Text style={styles.btnText}>Ir a detail</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  titleText: {
+    color: "white",
+    textAlign: "center",
+  },
+  btn: {
+    padding: 0,
+    backgroundColor: "blue",
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
