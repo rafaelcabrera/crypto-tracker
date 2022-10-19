0bb85e4ae700fd4eb6c05cd8b80c91a9b2e96f5b
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-18 23:09:21 -0300

    fech

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,12 +1,19 @@
 import  React, {Component } from 'react';
 import {View, Text, Pressable, StyleSheet} from 'react-native'
+import Http from '../../libs/http'
 
 class CoinsScreen extends Component {
-    handlePress = () => {
-        this.props.navigation.navigate('CoinDetail')
-        console.log("Click")
+    
+  componentDidMount = async () => {
+      const coins = await Http.instance.get("https://api.coinlore.net/api/tickers/");
+
+      console.log("coins", coins);
     }
 
+    handlePress = () => {
+      this.props.navigation.navigate('CoinDetail')
+      console.log("Click")
+  }
     render(){
         return(
             <View style={styles.container}>
@@ -20,6 +27,19 @@ class CoinsScreen extends Component {
         )
     }
 }
+// export const CoinsScreen = ({ navigation }) => {
+//   const handlePess = () => {
+//       navigation.navigate('CoinDetailScreen')
+//   }
+//   return (
+//       <View style={styles.container}>
+//           <Text style={styles.titleText}>CoinsScreen </Text>
+//           <Pressable style={styles.btn} onPress={handlePess}>
+//               <Text style={styles.btnText}>Ir a detail</Text>
+//           </Pressable>
+//       </View>
+//   )
+// }
 export default CoinsScreen
 
 const styles = StyleSheet.create({
