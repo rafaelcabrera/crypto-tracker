b7ce6acaff5ec6b98112eb1446f22ddb86d3ac02
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-18 21:43:53 -0300

    minor fix

--- src/components/coins/CoinDetailScreen.js
+++ src/components/coins/CoinDetailScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 
 class CoinDetailScreen extends Component {
   handlePress = () => {
