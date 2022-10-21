ac90164b78806f7dd24879715e1f76a75aca8d7f
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-21 02:06:11 -0300

    colors and list working

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,115 +1,40 @@
 import React, {Component} from 'react';
-import {
-  View,
-  Text,
-  Pressable,
-  ActivityIndicator,
-  StyleSheet,
-  TextComponent,
-} from 'react-native';
+import {View, Text, Pressable, StyleSheet, TextComponent} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import Http from '../../libs/http';
-import CoinsItem from './CoinsItem';
 
 class CoinsScreen extends Component {
+  state = {coins: []};
 
-  state = {
-    coins: [],
-    allCoins: [],
-    loading: false
-  }
+  componentDidMount = async () => {
+    const res = await Http.instance.get(
+      'https://api.coinlore.net/api/tickers/',
+    );
 
-  componentDidMount = () => {
+    this.setState({coins: res.data});
+  };
 
-    this.getCoins();
-
-  }
-
-  getCoins = async () => {
-    this.setState({ loading: true });
-
-    const res = await Http.instance.get("https://api.coinlore.net/api/tickers/");
-
-    this.setState({ coins: res.data, allCoins: res.data, loading: false });
-  }
-
-  handlePress = (coin) => {
-
-    this.props.navigation.navigate('CoinDetail', { coin });
-  }
-
-  handleSearch = (query) => {
-    const { allCoins } = this.state;
-
-    const coinsFiltered = allCoins.filter((coin) => {
-      return coin.name.toLowerCase().includes(query.toLowerCase()) ||
-        coin.symbol.toLowerCase().includes(query.toLowerCase());
-    });
-
-    this.setState({ coins: coinsFiltered });
-  }
-
+  handlePress = () => {
+    this.props.navigation.navigate('CoinDetail');
+    console.log('Click');
+  };
   render() {
-
-    const { coins, loading } = this.state;
+    const {coins} = this.state;
 
     return (
       <View style={styles.container}>
-
-      {/* <CoinsSearch onChange={this.handleSearch} /> */}
-
-        { loading ?
-          <ActivityIndicator
-            style={styles.loader}
-            color="#fff"
-            size="large"
-
-          />
-          : null
-        }
-
         <FlatList
           data={coins}
-          renderItem={({ item }) =>
-            <CoinsItem
-            item={item}
-            onPress={() => this.handlePress(item)}
-          />
-          }
-        />
+          renderItem={({item}) => (
+            <View>
+              <Text>{item.name}</Text>
+              <Text>{item.symbol}</Text>
+            </View>  
+          )}></FlatList>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: Colors.charade
-  },
-  titleText: {
-    color: "#fff",
-    textAlign: "center"
-  },
-  btn: {
-    padding: 8,
-    backgroundColor: "blue",
-    borderRadius: 8,
-    margin: 16
-  },
-  btnText: {
-    color: "#fff",
-    textAlign: "center"
-  },
-  loader: {
-    marginTop: 60
-  }
-});
-
-export default CoinsScreen;
-
-
-
 // export const CoinsScreen = ({ navigation }) => {
 //   const handlePess = () => {
 //       navigation.navigate('CoinDetailScreen')
@@ -123,3 +48,25 @@ export default CoinsScreen;
 //       </View>
 //   )
 // }
+export default CoinsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  tittleText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: '#0000aa',
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
